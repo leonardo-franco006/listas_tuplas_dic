fc4f1deeeb7035acf90d5f45024d0819e62e71d8
leonardo-franco006 <leo.franco2006@gmail.com>
2025-05-20 22:46:10 -0400

    Exercicios

---
+++ exercicios faceis/ex10.py
@@ -0,0 +1,6 @@
+# 10. Dada uma lista de palavras, junte todas elas em uma string separada por vırgulas.
+
+lista = ['palavra', 'palavrinha', 'palavrao', 'palavrona' ]
+
+string = ', '.join(lista)
+print(string)
--- exercicios faceis/ex9.py
+++ exercicios faceis/ex9.py
@@ -4,3 +4,7 @@
 lista = [3, 1, 4, 1, 5, 9]
 
 lista.sort()
+print(lista)
+
+lista.sort(reverse=True)
+print(lista)
